d8169ce87f89bac8d0118256fea4509a857d64b6
Kim Laï Trinh <kimlai@lrqdo.fr>
2015-02-17 11:38:39 +0100

    Make the dbal event store connection configurable.
    Will fix https://github.com/qandidate-labs/broadway/issues/3.
    
    A new configuration option is available :
    ```
    broadway:
        event_store:
            dbal:
                connection: sqlite
    ```
    It defaults to `null`, in which case the service `database_connection` is
    used (alias created by doctrine to get the default connection).

--- src/Broadway/Bundle/BroadwayBundle/Command/SchemaEventStoreCreateCommand.php
+++ src/Broadway/Bundle/BroadwayBundle/Command/SchemaEventStoreCreateCommand.php
@@ -11,7 +11,10 @@
 
 namespace Broadway\Bundle\BroadwayBundle\Command;
 
+use Broadway\EventStore\DBALEventStore;
 use Exception;
+use RuntimeException;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -72,5 +75,16 @@ EOT
         }
 
         return $error ? 1 : 0;
+    }
+
+    private function getEventStore()
+    {
+        $eventStore = $this->getContainer()->get('broadway.event_store');
+
+        if (! $eventStore instanceof DBALEventStore) {
+            throw new RuntimeException("'broadway.event_store' must be configured as an instance of DBALEventStore");
+        }
+
+        return $eventStore;
     }
 }
